f1612c27a042e4916e99d6c1c76fab9b3fec2a7d
Karthik <mskarthik6@gmail.com>
2021-12-29 17:43:13 +0530

    Math class added with tests.

---
+++ MyCircleCITestApp/Math.swift
@@ -0,0 +1,14 @@
+//
+//  Math.swift
+//  MyCircleCITestApp
+//
+//  Created by Karthik Maharajan Skandarajah on 29/12/21.
+//
+
+import Foundation
+
+class Math {
+    static func add(_ n1: Int, _ n2: Int) -> Int {
+        n1 + n2
+    }
+}
